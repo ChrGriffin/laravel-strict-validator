5e3ae88987b6f45fcaeeeac88b0b1c6aaa1179df
Christian Griffin <c.griffin@arcane.ws>
2018-05-15 13:04:02 -0400

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# laravel-strict-validator
-'Strict' validator for PHP arrays. Currently only supported in Laravel.
+'Strict' validator for PHP arrays. Currently only supported in Lumen/Laravel as it uses the Laravel validation implementation. Use this package to add 'strict' validation in addition to the normal Laravel validation rules. 
+
+## 'Strict' Rules
+
+1. Arrays can only contain fields under validation. Any 'extra' indexes will cause the validator to return `false.
+`
+2. _to be continued..?_
+
+## Installation
+
+```$xslt
+composer require chrgriffin/laravel-strict-validator
+```
+
+## Usage
+
+```
+use ChrGriffin\StrictValidator;
+ 
+$validator = new StrictValidator(
+    $dataToValidate,
+    [
+        'foo'       => 'string|required',
+        'bar'       => 'array|required',
+        'bar.inner' => 'string|required'
+    ]
+);
+
+$valid = $validator->validate();
+```
---
+++ StrictValidator.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace ChrGriffin;
+
+use Illuminate\Support\Facades\Validator;
+
+class StrictValidator
+{
+    /**
+     * Data to validate.
+     *
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * Structure to validate the data against.
+     *
+     * @var array
+     */
+    protected $structure = [];
+
+    /**
+     * StrictValidator constructor.
+     *
+     * @param array $data Data to validate.
+     * @param array $structure Structure to validate the data against.
+     * @return void
+     */
+    public function __construct(array $data, array $structure)
+    {
+        $this->data = $data;
+        $this->structure = $structure;
+    }
+
+    /**
+     * Validate all the given data against the given structure.
+     *
+     * @return bool
+     */
+    public function validate() : bool
+    {
+        $validator = Validator::make(
+            $this->data, $this->structure
+        );
+
+        if($validator->fails()) {
+            return false;
+        }
+
+        return $this->validateArray($this->data, $this->structure);
+    }
+
+    /**
+     * Validate a specific given array against the structure for that specific array.
+     *
+     * @param array $data
+     * @param array $structure
+     * @return bool
+     */
+    protected function validateArray(array $data, array $structure) : bool
+    {
+        // the array can't contain any values we're not expecting
+        if(!empty(array_diff_key($data, $structure))) {
+            return false;
+        }
+
+        // recursively perform the same validation on any nested arrays
+        foreach($data as $dataIndex => $dataValue) {
+            if(is_array($dataValue)) {
+                $nestedStructure = [];
+                $regex = '/^' . $dataIndex . '.(\w+)/';
+
+                foreach($structure as $structureIndex => $structureValue) {
+                    if(preg_match($regex, $structureIndex)) {
+                        $newIndex = preg_replace($regex, '$1', $structureIndex);
+                        $nestedStructure[$newIndex] = $structureValue;
+                    }
+                }
+
+                if(!$this->validateArray($dataValue, $nestedStructure)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
